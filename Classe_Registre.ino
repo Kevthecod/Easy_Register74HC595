5d77d42d8ce34329c0dac6f40b97f7e735ef9be9
Kevthecod <140403363+Kevthecod@users.noreply.github.com>
2024-03-14 12:07:45 +0100

    Add files via upload

---
+++ Classe_Registre.ino
@@ -0,0 +1,30 @@
+#include "Register.h"
+
+Register myRegis(5, 6, 7, 1);
+
+
+void setup() {
+  Serial.begin(9600);
+  myRegis.SetRegPin();
+  myRegis.initOut();
+}
+
+void loop() {
+  for (int lead = 0; lead <= 500; lead+=2)
+  {
+    Serial.print("Ecart  = ");
+    Serial.println(lead);
+    for (int i = 0; i < 8; i++)
+    {
+      myRegis.Set(i);
+      delay(lead);
+      //myRegis.Reset(i);
+    }
+    for (int i = 0; i < 8; i++) 
+    {
+      myRegis.Reset(7-i);
+      delay(lead);
+      //myRegis.Reset(7-i);
+    }
+  }
+}
